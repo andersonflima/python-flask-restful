63dbcc5de097e467fd0099d5bfe5065122f83a90
Anderson F Lima <andersonfaustinolima@gmail.com>
2021-01-11 02:00:23 -0300

    port change

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask_restful import Api, Resource, request
 
-
 app = Flask(__name__)
 api = Api(app)
 
 class Hello(Resource):
-    def get():
+    def get(self):
         if "/" == request.path:
             return 'Hello Wolrd!'
             
 api.add_resource(Hello, "/")
 
 if __name__=='__main__':
-    app.run('localhost', port=3000)
+    app.run('localhost', port=3002)
